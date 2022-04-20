a34722a048c9286d42324e5edccfb44015dcca6d
Arno <arno@elephantcs.nl>
2022-04-20 20:17:36 +0200

    Add user section

--- src/scripts/vue/components/Users.vue
+++ src/scripts/vue/components/Users.vue
@@ -1,9 +1,21 @@
 <template>
-    <div class="p-8 overflow-auto">
-        <h2 class="text-2xl font-bold">Users</h2>
+    <div class="p-8 flex flex-col justify-between h-full">
+        <div>
+            <h2 class="text-2xl font-bold mb-6">Users</h2>
+            <div>
+                <div class="mb-4 relative" v-for="index in 5" :key="index">
+                    <User />
+                    <span v-if="index === 1" class="absolute w-full h-px -top-2 bg-grey-700"></span>
+                    <span class="absolute w-full h-px -bottom-2 bg-grey-700"></span>
+                </div>
+            </div>
+        </div>
+        <div>
+            Login / Sign up Modal
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-    
+    import User from '../parts/User.vue';
 </script>
---
+++ src/scripts/vue/parts/User.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex items-center">
+        <figure class="relative mr-3">
+            <img src="/placeholders/user.jpg" alt="" class="w-10 h-10 rounded-full">
+            <span class="status absolute bottom-0 right-0 w-2.5 h-2.5 rounded-full bg-lime-500 ring-4 ring-grey-800"></span>
+        </figure>
+        <span class="">Horsefighter</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+    
+</script>
