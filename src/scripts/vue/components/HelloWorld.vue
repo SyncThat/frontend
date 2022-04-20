450b7bd6f3de14d22d8b31e90ff6fecd50ed4da9
Tom Harms <tom@elephantcs.nl>
2022-04-20 09:50:01 +0200

    Add eslint and prettier config

--- src/scripts/vue/components/HelloWorld.vue
+++ src/scripts/vue/components/HelloWorld.vue
@@ -1,29 +1,37 @@
 <script setup lang="ts">
-    import { ref } from 'vue'
+import { ref } from 'vue';
 
-    defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-    const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
-    <div class="grid grid-cols-4 gap-4">
-        <div>
-            <h1 class="text-3xl font-bold">Queue</h1>
-        </div>
-        
-        <div>
-            <h1 class="text-3xl font-bold">Chat</h1>
+	<div class="grid grid-cols-4 gap-4">
+		<div>
+			<h1 class="text-3xl font-bold">Queue</h1>
+		</div>
 
-            <div>
-                <input class="bg-gray-200 text-black py-2 px-4 rounded" type="text" placeholder="Type your message">
+		<div>
+			<h1 class="text-3xl font-bold">Chat</h1>
 
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click me</button>
-            </div>
-        </div>
+			<div>
+				<input
+					class="bg-gray-200 text-black py-2 px-4 rounded"
+					type="text"
+					placeholder="Type your message"
+				/>
 
-        <div>
-            <h1 class="text-3xl font-bold">People</h1>
-        </div>
-    </div>
+				<button
+					class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+				>
+					Click me
+				</button>
+			</div>
+		</div>
+
+		<div>
+			<h1 class="text-3xl font-bold">People</h1>
+		</div>
+	</div>
 </template>
