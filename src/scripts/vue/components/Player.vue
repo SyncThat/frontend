5e5a7362637eb0ddb0170468417e1f45bd3792ca
Tom Harms <tom@elephantcs.nl>
2022-04-20 22:31:05 +0200

    Add player styling

--- src/scripts/vue/components/Player.vue
+++ src/scripts/vue/components/Player.vue
@@ -1,15 +1,23 @@
 <template>
-    <div class="h-1/3 p-8">
-        <div>
-            <div class="mb-6" id="wave" ref="waveElement"></div>
-
-            <div class="flex gap-2">
+    <div class="flex gap-4 p-8 bg-grey-900">
+        <div class="flex flex-col justify-between w-2/3 my-6">
+            <div class="flex gap-2 mb-6">
                 <Button @click="playHandler()">Play</Button>
 
                 <Button @click="pauseHandler()">Pause</Button>
             </div>
-            
+
+            <div class="mb-8">
+                <span class="block">Rick Astley</span>
+                <span class="block text-grey-500 text-sm">Never Gonna Give You Up</span>
+            </div>
+
+            <div class="mb-6" id="wave" ref="waveElement"></div>
         </div>
+
+        <figure class="relative w-1/3">
+            <img src="/placeholders/cover.jpg" alt="" class="w-full">
+        </figure>
     </div>
 </template>
 
@@ -50,6 +58,9 @@
 
         getApi(headers, '/placeholder.json').then(peaks => {
             waveSurfer.load('https://stoux.nl/music/MainConcernQBaseXtraRaw.mp3', peaks.data, 'metadata')
+
+            console.log(waveSurfer.getDuration());
+            
         }).catch((e) => {
             console.error('error', e);
         });
