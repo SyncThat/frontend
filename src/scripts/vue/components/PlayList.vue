12edcfb1d8124f196ce949ab6052eb91b938621a
Arno <arno@elephantcs.nl>
2022-04-20 22:16:20 +0200

    Changes to Playlist.vue

--- src/scripts/vue/components/PlayList.vue
+++ src/scripts/vue/components/PlayList.vue
@@ -1,17 +1,14 @@
 <template>
-    <div class="p-8 flex flex-col justify-between h-full">
-        <div>
-            <h2 class="text-3xl font-bold mb-8">Playlist</h2>
-            <div>
-                <div class="mb-6 relative" v-for="index in 10" :key="index">
-                    <Song />
-                    <span v-if="index === 1" class="absolute w-full h-px -top-3 bg-grey-700"></span>
-                    <span class="absolute w-full h-px -bottom-3 bg-grey-700"></span>
-                </div>
+    <div class="flex flex-col justify-between h-full">
+        <h2 class="px-8 py-4 text-3xl font-bold bg-grey-900">Playlist</h2>
+        <div class="h-full px-8 pt-4 overflow-auto">
+            <div class="mb-6 relative" v-for="index in 20" :key="index">
+                <Song />
+                <span class="absolute w-full h-px -bottom-3 bg-grey-700"></span>
             </div>
         </div>
-        <div class="flex">
-            <Button :title="'Sync that!'"/>
+        <div class="flex py-4 px-8 bg-grey-900">
+            <Button>Sync that!</Button>
         </div>
     </div>
 </template>
