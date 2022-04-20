e75c6db5bbc30cb79890f1e4bdac521f4fd06033
Tom Harms <tom@elephantcs.nl>
2022-04-20 23:05:29 +0200

    Add data body postApi

--- src/scripts/ts/helpers/postApi.ts
+++ src/scripts/ts/helpers/postApi.ts
@@ -2,14 +2,15 @@
     /*  post API ASYNC function
     /*  postApi(headers, url);
 /*------------------------------------------------------------------------*/
-export default async function postApi(headers:HeadersInit, url:RequestInfo) {
+export default async function postApi(headers:HeadersInit, url:RequestInfo, data:Object) {
     try {
         const response = await fetch(url, {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, *cors, same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             credentials: 'same-origin', // include, *same-origin, omit,
-            headers
+            headers,
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
         });
 
 		if (!response.ok) {
