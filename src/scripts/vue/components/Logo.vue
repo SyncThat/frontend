874eaf01894e8316c22a6f590080ff2852473a62
Tom Harms <tom@elephantcs.nl>
2022-05-19 22:18:00 +0200

    Add logo

---
+++ src/scripts/vue/components/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex items-center justify-center bg-blue-600 p-8">
+        <img src="/images/logo.svg" alt="" class="w-56">
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
