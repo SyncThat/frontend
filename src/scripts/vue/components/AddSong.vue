c54f5183ea1a0efa8c08ba2bd69f819d859fd3d5
Tom Harms <tom@elephantcs.nl>
2022-05-23 18:01:35 +0200

    Replace elipsis

--- src/scripts/vue/components/AddSong.vue
+++ src/scripts/vue/components/AddSong.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col gap-4 relative py-8 px-6 z-10 before:content[''] before:absolute before:bottom-0 before:left-0 before:h-[150%] before:w-full before:-z-10 before:bg-gradient-to-t before:from-cyan-900 before:to-cyan-900/0">
         <div class="flex items-center gap-4 w-full rounded-lg text-gray-200 ring-1 p-2 ring-gray-400">
             <div>
-                <input type="text" id="song-url" name="song-url" placeholder="Paste your song URL..." v-model="songURL" class="pl-4 appearance-none w-full bg-transparent focus:outline-none"/>
+                <input type="text" id="song-url" name="song-url" placeholder="Paste your song URL…" v-model="songURL" class="pl-4 appearance-none w-full bg-transparent focus:outline-none"/>
             </div>
 
             <div class="shrink-0 ml-auto">
